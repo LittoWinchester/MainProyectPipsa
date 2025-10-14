9a8527620b99721d55aef3be3fb25df21b860c48
Litto <Litto_fernandez@hotmail.com>
2025-10-14 19:33:26 -0300

    Agregado todo lo de LAN
    se crea el WBP de lan en Rider.

---
+++ Source/MainProyect/Private/WidgetLan/MenuLan.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WidgetLan/MenuLan.h"
+
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Kismet/GameplayStatics.h"
+
+void UMenuLan::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	FInputModeUIOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(true);
+	
+	Button_Host->OnClicked.AddDynamic(this,&UMenuLan::OnClicked_Host);
+	Button_Join->OnClicked.AddDynamic(this,&UMenuLan::OnClicked_Join);	
+	
+}
+
+void UMenuLan::OnClicked_Host()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this,World_LanWorld,true,TEXT("listen"));
+	
+	
+}
+
+void UMenuLan::OnClicked_Join()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	const FString IP=EditableTextBox_IPtoJoin->GetText().ToString();
+
+	UGameplayStatics::OpenLevel(this,*IP);
+	
+	
+}
---
+++ Source/MainProyect/Public/WidgetLan/MenuLan.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuLan.generated.h"
+
+class UEditableTextBox;
+class UButton;
+/**
+ * 
+ */
+UCLASS()
+class MAINPROYECT_API UMenuLan : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeOnInitialized() override;
+private:
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UButton> Button_Host;
+	
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UButton> Button_Join;
+
+	UPROPERTY(Meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> EditableTextBox_IPtoJoin;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> World_LanWorld;
+
+	UFUNCTION()
+	void OnClicked_Host();
+
+	UFUNCTION()	
+	void OnClicked_Join();
+	
+	
+};
